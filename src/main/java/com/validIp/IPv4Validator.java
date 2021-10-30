88ea5669e7e4e3aea7a41bbc8e6f63fc6d5dd659
Evgen <evgenlandarenko@gmail.com>
2021-10-30 23:15:47 +0300

    Update and rename IPv4ValidatorRegex.java to IPv4Validator.java

--- src/main/java/com/validIp/IPv4Validator.java
+++ src/main/java/com/validIp/IPv4Validator.java
@@ -11,7 +11,7 @@ package com.validIp;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class IPv4ValidatorRegex {
+public class IPv4Validator {
 
     private static final String IPV4_PATTERN =
             "^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}$";
